8706f8a4e01fc6b7dd173259c11096533f99fafa
Flávio França <flavio.hmmf90@gmail.com>
2023-11-02 20:14:32 -0300

    adjustments to adaptation from carrossel1

--- carrossel.js
+++ carrossel.js
@@ -2,6 +2,8 @@
 const  imgs = document.getElementById("img"); 
 const img = document.querySelectorAll("#img img");
 
+
+
 // Contador que se inicia em 0
 let id_imgs = 0;
 
@@ -14,12 +16,21 @@ function carrossel(){
     if(id_imgs > img.length - 1){
         id_imgs = 0;
     }
+
+    // Pega o tamanho da imagem
+    const imgSize = img[id_imgs].offsetWidth;
+    
+    // ajeita a imagem pro tamanho
+    img[id_imgs].style.width = imgSize;
+
     // Animação de passagem ( o quanto a variável irá passar para a direita)
-    imgs.style.transform = `translatex(${-id_imgs * 599}px)`;
-    
-    
+    imgs.style.transform = `translatex(${-id_imgs * imgSize}px)`;
+    if (imgSite < 680){
+        imgs.style.transform = `translatex(${-id_imgs * 499}px)`;
+    }
 
 }  
+
 // chamando a função do carrossel e adicionando o tempo em ms (2000ms = 2s)
 setInterval(carrossel,2000);
 
